0256ba25d6decb83ba61108de4b4d5d8dba4d3b1
Vincent Demaurex <vincent@demaurex.fr>
2014-05-01 15:20:51 +0200

    Taille des Logos

--- protected/views/site/_logosPlateforme.php
+++ protected/views/site/_logosPlateforme.php
@@ -1,18 +1,20 @@
     <div class="hidden-xs hidden-sm"> 
         <hr/>
-        <h2>Accès direct aux principales plateformes</h2>
-        <table class="centpp" cellspacing="10" cellpadding="0" border="0"><tbody><tr><td>
+        <h3>Accès direct aux principales plateformes</h3>
+        <div style="margin: auto;">
+        <table class="centpp" cellspacing="10" cellpadding="0" border="0" ><tbody><tr><td>
 
-                        <a target="_blank" href="http://www.sciencedirect.com/"><img width="180" title="Elsevier Science Direct" src="<?= Yii::app()->baseUrl; ?>/images/sciencedirect.png"></a>
+                        <a target="_blank" href="http://www.sciencedirect.com/"><img width="170" title="Elsevier Science Direct" src="<?= Yii::app()->baseUrl; ?>/images/sciencedirect.png"></a>
                     </td><td>
-                        <a target="_blank" href="http://www.tandfonline.com/"><img width="60" title="Taylor &amp; Francis Online" src="<?= Yii::app()->baseUrl; ?>/images/tandfonline.jpg"></a>
+                        <a target="_blank" href="http://www.tandfonline.com/"><img width="50" title="Taylor &amp; Francis Online" src="<?= Yii::app()->baseUrl; ?>/images/tandfonline.jpg"></a>
                     </td><td>
-                        <a target="_blank" href="http://onlinelibrary.wiley.com/"><img width="150" title="Wiley" src="<?= Yii::app()->baseUrl; ?>/images/wiley.jpg"></a>
+                        <a target="_blank" href="http://onlinelibrary.wiley.com/"><img width="140" title="Wiley" src="<?= Yii::app()->baseUrl; ?>/images/wiley.jpg"></a>
                     </td><td>
-                        <a target="_blank" href="http://www.springer.com/"><img width="160" title="Springer" src="<?= Yii::app()->baseUrl; ?>/images/springer.jpg"></a>
+                        <a target="_blank" href="http://www.springer.com/"><img width="150" title="Springer" src="<?= Yii::app()->baseUrl; ?>/images/springer.jpg"></a>
                     </td><td>
-                        <a target="_blank" href="http://www.jstor.org/"><img width="60" title="JSTOR" src="<?= Yii::app()->baseUrl; ?>/images/jstor.jpg"></a>
+                        <a target="_blank" href="http://www.jstor.org/"><img width="50" title="JSTOR" src="<?= Yii::app()->baseUrl; ?>/images/jstor.jpg"></a>
                     </td><td>
-                        <a target="_blank" href="http://www.cairn.info/"><img width="243" title="Cairn.info" src="<?= Yii::app()->baseUrl; ?>/images/CAIRN.INFO.png"></a>
+                        <a target="_blank" href="http://www.cairn.info/"><img width="233" title="Cairn.info" src="<?= Yii::app()->baseUrl; ?>/images/CAIRN.INFO.png"></a>
                     </td></tr></tbody></table>
     </div>
+        </div>
