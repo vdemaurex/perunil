e50db20a7ba82c611beb75e745059f77f6039a70
Vincent Demaurex <vincent@demaurex.fr>
2014-06-03 09:09:37 +0200

    Correction HTML5

--- protected/views/layouts/main_btp3.php
+++ protected/views/layouts/main_btp3.php
@@ -2,7 +2,6 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="language" content="fr" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 
