fdf15156fca6167e42ff4145b8c0851df69b271c
Vincent Demaurex <vincent@demaurex.fr>
2014-07-31 15:11:20 +0200

    Mapping search => site/simpleSearchResults

--- protected/config/main.php
+++ protected/config/main.php
@@ -44,6 +44,7 @@ return array(
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<action:(search)>' => 'site/simpleSearchResults',
             ),
         ),
         /*
