19ee47ef2e45de58a5740fbc7e96a3947913772d
Vincent Demaurex <vincent@demaurex.fr>
2014-09-09 16:22:29 +0200

    Meilleure présentation de la grille du lot à modifier

--- protected/views/admin/gridviewdialog.php
+++ protected/views/admin/gridviewdialog.php
@@ -1,9 +1,49 @@
+<!DOCTYPE html> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <!-- Bootstrap -->
+        <link href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap.min.css" rel="stylesheet" media="screen" /> 
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+
+        <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+        <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/favicon.ico" />
+        <title>Affichage du lot</title>
+
+        <?php
+        // Ajout de la librairie javascript jquery
+        Yii::app()->clientScript->registerCoreScript('jquery');
+        Yii::app()->clientScript->registerCoreScript('jquery.ui');
+
+        // ajout de la css de jquery ui
+        Yii::app()->clientScript->registerCssFile(Yii::app()->clientScript->getCoreScriptUrl() . '/jui/css/base/jquery-ui.css');
+
+
+        // Autocompletion lors de la recherche
+        Yii::app()->clientScript->registerCoreScript('autocomplete');
+        ?>
+
+        <script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap.min.js"></script>
+
+    </head>
+
+    <body>
 <?php
 // Ne peut fonctionner qu'avec les résultat de la recherche admin.
 if(Yii::app()->session['searchtype'] != 'admin'){
     throw new Exception("Merci d'utiliser l'exporation CSV depuis les résultats de la recherche administrateur uniquement.");
 }
-$dataProvider = Yii::app()->session['search']->admin_dp;
+$dataProvider = Yii::app()->session['search']->admin_adp;
 //
 // affichage par abonnements
 //
@@ -86,5 +126,8 @@ $this->widget('zii.widgets.grid.CGridView', array(
     'formatter' => new FrFormatter(),
     //'filter'=>  Abonnement::model(),
     'columns' => $columns,
-));
+    //'htmlOptions' => array('class' => 'table table-striped')
+)); 
 ?>
+</body>
+</html>
