1f8a0326ccc623777a526adf3ede11f9843819d4
Vincent Demaurex <vincent@demaurex.fr>
2014-11-05 11:36:03 +0100

    Nouvelle version

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,7 @@
 <?php
 
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../lib/yii-1.1.10.r3566/framework/yiic.php';
+$yiic=dirname(__FILE__).'/../../lib/yii-1.1.15.022a51/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
